49fd201e53be9956fc7053aaabaa98744d4fc78c
Morten Houston Ludvigsen <mlu@2ps.dk>
2014-08-27 23:29:26 +0200

    Added a few tests in typescript

---
+++ Demo/test/TypescriptTests.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="../typings/jasmine/jasmine.d.ts" />
+
+describe("The test explorer", function () {
+    it("should link to the original typescript file", function () {
+    });
+});
+
+
+describe("This test", function () {
+    it("should succeed", function () {
+        expect(true).toBe(true);
+    });
+
+    it("should fail", function () {
+        expect(true).toBe(false);
+    });
+});
