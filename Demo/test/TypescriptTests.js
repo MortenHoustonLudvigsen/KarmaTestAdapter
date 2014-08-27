c1daaaa5530aed32fe797b38a1eb1bd95c3c63f8
Morten Houston Ludvigsen <mlu@2ps.dk>
2014-08-27 23:35:56 +0200

    Added the generated js files to the project

---
+++ Demo/test/TypescriptTests.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../typings/jasmine/jasmine.d.ts" />
+describe("The test explorer", function () {
+    it("should link to the original typescript file", function () {
+    });
+});
+
+describe("This test", function () {
+    it("should succeed", function () {
+        expect(true).toBe(true);
+    });
+
+    it("should fail", function () {
+        expect(true).toBe(false);
+    });
+});
+//# sourceMappingURL=TypescriptTests.js.map
