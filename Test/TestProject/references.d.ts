9955d7d87e4dc693ca80b53477a089eb9d5d160e
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-02-05 10:53:59 +0100

    Add test project

---
+++ Test/TestProject/references.d.ts
@@ -0,0 +1,2 @@
+﻿/// <reference path="typings/tsd.d.ts" />
+ 
---
+++ Test/TestProject/test/TestFile2.js
@@ -0,0 +1,18 @@
+﻿describe('Simple tests 2', function () {
+    it('should be, that 3 + 2 = 4', function () {
+        expect(3 + 2).toBe(4);
+    });
+    it('should be, that 3 + 12 = 23', function () {
+        expect(3 + 12).toBe(23);
+    });
+    it('should be, that 3 + 12 = 23', function () {
+        expect(3 + 12).toBe(15);
+    });
+    it('should be, that 3 + 96 = 23', function () {
+        expect(3 + 12).toBe(23);
+    });
+    it('should be, that 3 + 12 = 23', function () {
+        expect(3 + 12).toBe(23);
+    });
+});
+//# sourceMappingURL=TestFile2.js.map
