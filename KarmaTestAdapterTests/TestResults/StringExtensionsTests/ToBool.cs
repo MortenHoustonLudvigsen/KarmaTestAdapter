3fb4f3070c23acbcd24911dcf9580edbe831b2cf
Morten Houston Ludvigsen <mlu@2ps.dk>
2015-02-10 13:06:16 +0100

    Rename folder and namespace

--- KarmaTestAdapterTests/TestResults/StringExtensionsTests/ToBool.cs
+++ KarmaTestAdapterTests/TestResults/StringExtensionsTests/ToBool.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace KarmaTestAdapterTests.TestResults.StringExtensions
+namespace KarmaTestAdapterTests.TestResults.StringExtensionsTests
 {
     public partial class TestResults
     {
